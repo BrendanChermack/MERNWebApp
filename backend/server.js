dfb88fa67b6a02621c59b00048948cfb770aaa82
Brendan <chermackbrendan@gmail.com>
2023-02-21 22:35:29 -0600

    start

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middeware allows parsing of json to the server
+app.use(cors());
+app.use(express.json());
+
+mongoose.set("strictQuery", false);
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+
+//mongoDB connection 
+connection.once('open', () => {
+    console.log('MongoDB database connection established');
+})
+
+const exercisesRouter = require('./routes/exercises');
+const userRouter = require('./routes/user');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', userRouter);
+
+//starts the server
+app.listen(port, () => 
+{
+    console.log(`Server is running on port: ${port}`);
+});
