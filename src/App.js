eba41df0dfb7c1e4c12a67246342c90d9cd5bda3
Brendan <chermackbrendan@gmail.com>
2023-03-01 15:49:27 -0600

    app.js edited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Router>    
+      <Navbar/> 
+      <br/> 
+      <Routes> 
+      <Route path="/" exact element={<ExercisesList />} /> 
+      <Route path="edit/:id" element={<EditExercise />} /> 
+      <Route path="create" element={<CreateExercise />} /> 
+      <Route path="user" element={<CreateUser />} /> 
+      </Routes>
+    </Router>
+
   );
 }
 
