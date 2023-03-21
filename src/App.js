3a05f1e0075b43b62b7e7593282de75e2419c54a
Brendan <chermackbrendan@gmail.com>
2023-03-20 20:36:24 -0500

    components added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar.component";
+import ExerciseList from "./components/exercise-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 //npm start to start web
 function App() {
   return (
-    <Router>    
-      <navbar>
-      <br/>
-      <route path="/" exact component={ExerciseList} />
-      <route path="/edit/:id" component={EditExercise} />
-      <route path="/create" component={CreateExercise} />
-      <route path="/user" component={CreateUser} />
-      </navbar>
+    <Router> 
+    <Navbar /> 
+    <br/> 
+        <Routes> 
+            <Route path="/" exact component={<ExerciseList />} /> 
+            <Route path="edit/:id" component={<EditExercise />} /> 
+            <Route path="create" component={<CreateExercise />} /> 
+            <Route path="user" component={<CreateUser />} /> 
+        </Routes>
     </Router>
 
   );
