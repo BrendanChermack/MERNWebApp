c27cf1dc1006bc04523bd4453ee826032481a79f
Brendan <chermackbrendan@gmail.com>
2023-03-20 20:06:06 -0500

    new changes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middeware allows parsing of json to the server
+// middeware allows parsing of json to the server              ////// to start server npm start serve
 app.use(cors());
 app.use(express.json());
 
--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+//npm start to start web
 function App() {
   return (
     <Router>    
-      <Navbar/> 
-      <br/> 
-      <Routes> 
-      <Route path="/" exact element={<ExercisesList />} /> 
-      <Route path="edit/:id" element={<EditExercise />} /> 
-      <Route path="create" element={<CreateExercise />} /> 
-      <Route path="user" element={<CreateUser />} /> 
-      </Routes>
+      <navbar>
+      <br/>
+      <route path="/" exact component={ExerciseList} />
+      <route path="/edit/:id" component={EditExercise} />
+      <route path="/create" component={CreateExercise} />
+      <route path="/user" component={CreateUser} />
+      </navbar>
     </Router>
 
   );
